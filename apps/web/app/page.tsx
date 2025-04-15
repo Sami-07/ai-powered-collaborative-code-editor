073124d9c387d2ed086b78a3fef1014c3eceb5ef
Sami-07 <s.a.sami359359@gmail.com>
2025-04-15 10:26:50 +0530

    chore:

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -22,12 +22,13 @@ interface Feature {
   icon: string;
   title: string;
   description: string;
+
 }
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gradient-to-b from-gray-50 to-gray-100 overflow-hidden relative">
-      <Navbar />
+      
 
       {/* Background patterns */}
       <DotPattern className="absolute inset-0 opacity-50" />
